b0819a02c24bb171b103fed75c2489ea1a485b72
flatypus [mac] <yhc3141@gmail.com>
2023-11-20 00:19:02 -0800

    feat: basic api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { cors } from "@elysiajs/cors";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(4000);
+const app = new Elysia();
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+  })
+);
+
+app
+  .get("/:id", ({ params: { id }, request }) => {
+    return `Hello Elysia ${id} ${request.headers.get(
+      "user-agent"
+    )} ${request.headers.get("x-forwarded-for")}`;
+  })
+  .listen(4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
