c820856ffd2cd88258403554805d063fa3f0f1b0
flatypus [mac] <yhc3141@gmail.com>
2023-11-20 00:37:11 -0800

    feat: okay returns invisible image?

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,11 +9,23 @@ app.use(
   })
 );
 
+const transparentPngBuffer = Buffer.from(
+  "89504e470d0a1a0a0000000d49484452000000010000000108060000001f15c489000000017352474200aece1ce90000000b494441541857636000020000050001aad5c8510000000049454e44ae426082",
+  "hex"
+);
+
 app
   .get("/:id", ({ params: { id }, request }) => {
-    return `Hello Elysia ${id} ${request.headers.get(
-      "user-agent"
-    )} ${request.headers.get("x-forwarded-for")}`;
+    console.log(
+      `Hello Elysia ${id} ${request.headers.get(
+        "user-agent"
+      )} ${request.headers.get("x-forwarded-for")}`
+    );
+    return new Response(transparentPngBuffer, {
+      headers: {
+        "content-type": "image/png",
+      },
+    });
   })
   .listen(4000);
 
