f71893f49728b91a5ffbe4f320b70b96fc9c5c2b
flatypus [mac] <yhc3141@gmail.com>
2023-11-20 01:20:37 -0800

    feat: logging to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,22 @@
 import { Elysia } from "elysia";
 import { cors } from "@elysiajs/cors";
+import { config } from "dotenv";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import fetch from "node-fetch";
+import { pgTable, serial, text } from "drizzle-orm/pg-core";
+
+config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
+
+if (!DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const queryClient = postgres(DATABASE_URL);
+
+const db = drizzle(queryClient);
 
 const app = new Elysia();
 app.use(
@@ -14,13 +31,56 @@ const transparentPngBuffer = Buffer.from(
   "hex"
 );
 
+type IPInfo = {
+  query: string;
+  status: string;
+  country: string;
+  countryCode: string;
+  region: string;
+  regionName: string;
+  city: string;
+  zip: string;
+  lat: number;
+  lon: number;
+  timezone: string;
+  isp: string;
+  org: string;
+  as: string;
+};
+
+export const views = pgTable("views", {
+  id: serial("id").primaryKey(),
+  path: text("path"),
+  ip: text("ip"),
+  country: text("country"),
+  region: text("region"),
+  city: text("city"),
+  latitude: text("latitude"),
+  longitude: text("longitude"),
+  isp: text("isp"),
+  user_agent: text("user_agent"),
+});
+
 app
-  .get("/:id", ({ params: { id }, request }) => {
-    console.log(
-      `Hello Elysia ${id} ${request.headers.get(
-        "user-agent"
-      )} ${request.headers.get("x-forwarded-for")}`
-    );
+  .get("/:id", async ({ params: { id }, request }) => {
+    const user_agent = request.headers.get("user-agent");
+    const ip = request.headers.get("x-forwarded-for");
+    const ip_info = await fetch(`http://ip-api.com/json/${ip}`);
+    const ip_info_json = (await ip_info.json()) as IPInfo;
+    const { country, region, city, lat, lon, isp } = ip_info_json;
+
+    db.insert(views).values({
+      path: id,
+      ip: ip,
+      country: country,
+      region: region,
+      city: city,
+      latitude: lat.toString(),
+      longitude: lon.toString(),
+      isp: isp,
+      user_agent: user_agent,
+    });
+
     return new Response(transparentPngBuffer, {
       headers: {
         "content-type": "image/png",
