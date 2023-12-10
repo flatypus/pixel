c75993f60717a38e50eb79cf34ef9ab4225ded09
flatypus [mac] <yhc3141@gmail.com>
2023-12-10 04:53:56 -0800

    fix: okay we try vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   trailingSlash: "never",
   integrations: [tailwind(), react()],
-  adapter: node({
-    mode: "standalone"
-  }),
+  adapter: vercel(),
   server: {
     host: "0.0.0.0",
-    port: 3000
-  }
+    port: 3000,
+  },
 });
