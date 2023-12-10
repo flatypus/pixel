9ff9180614ef84d008a07982f2eddb49e0b2cb93
flatypus [mac] <yhc3141@gmail.com>
2023-12-10 03:15:16 -0800

    fix: output: server

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   trailingSlash: "never",
   integrations: [tailwind()],
   adapter: node({
