3d1bf108809271e809648f26b39543b4d134327c
flatypus [mac] <yhc3141@gmail.com>
2023-12-10 05:13:09 -0800

    fix: back to node

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import vercel from "@astrojs/vercel/serverless";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   trailingSlash: "never",
   integrations: [tailwind(), react()],
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
   server: {
     host: "0.0.0.0",
   },
